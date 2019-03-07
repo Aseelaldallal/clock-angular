b79e94f5a143540c979ae08cbad5573e5c201a6c
Aseel <aseelaldallal@gmail.com>
2019-03-06 22:10:03 -0500

    server

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/clock'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/clock/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
